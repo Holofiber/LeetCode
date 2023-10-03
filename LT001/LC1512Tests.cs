eafa37cd8753630458400c64b112f83abb182c80
Stepan Prots <stepan.prots@eleks.com>
2023-10-03 13:23:39 +0200

    LC1512 Easy

---
+++ LT001/LC1512Tests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTest
+{
+    internal class LC1512Tests
+    {
+        [Test]
+        [TestCase(4, new int[] { 1, 2, 3, 1, 1, 3 })]
+        [TestCase(6, new int[] { 1, 1, 1, 1 })]
+        [TestCase(0, new int[] { 1, 2, 3 })]
+        public void GoodPairs_ShouldReturn4(int expectredResult, int[] nums)
+        {
+            var GP = new LC1512_Number_of_good_pairs();
+
+            var result = GP.NumIdenticalPairs(nums);
+
+            Assert.AreEqual(expectredResult, result);
+        }
+    }
+}
---
+++ LeetCode/LC1512_Number_of_good_pairs.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace LeetCode
+{
+    public class LC1512_Number_of_good_pairs
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int goodPairs = 0;
+
+            foreach (int num in nums.Distinct().ToList())
+            {
+                int pairs = nums.Where(x => x.Equals(num)).Count();
+
+                goodPairs += ((pairs - 1) * pairs / 2);
+            }
+
+            return goodPairs;
+        }
+    }
+}
