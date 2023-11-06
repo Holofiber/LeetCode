85102bc282e976d3668b55bd122d7d08989b086f
Holofiber <aminus2010@gmail.com>
2023-11-06 18:18:35 +0100

    LC1245 works with array, but needs better structure, for improve time

---
+++ LT001/LC1245Tests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Medium;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCodeTest
+{
+    internal class LC1245Tests
+    {
+        [Test]
+        public void SeatManager_Test()
+        {
+            var st = new SeatManager(5);
+
+            List<int> answers = new List<int>();
+
+            answers.Add(st.Reserve());
+            answers.Add(st.Reserve());
+            st.Unreserve(2);
+            answers.Add(st.Reserve());
+            answers.Add(st.Reserve());
+            answers.Add(st.Reserve());
+            answers.Add(st.Reserve());
+            st.Unreserve(5);
+
+            var expected = new List<int>() { 1, 2, 2, 3, 4, 5 };
+
+            Assert.AreEqual(expected, answers);
+        }
+    }
+}
---
+++ LeetCode/Medium/LC1245.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Medium
+{
+    // https://leetcode.com/problems/seat-reservation-manager/
+    public class SeatManager
+    {
+        private int[] seats;
+
+        public SeatManager(int n)
+        {
+            seats = new int[n];
+        }
+
+        public int Reserve()
+        {
+            return GetFreeSeat();
+
+        }
+
+        public void Unreserve(int seatNumber)
+        {
+            int index = Array.IndexOf(seats, seatNumber);
+
+            seats[index] = 0;
+        }
+
+        private int GetFreeSeat()
+        {
+            for (int i = 0; i < seats.Length; i++)
+                if (seats[i] == 0)
+                {
+                    seats[i] = i+1;
+                    return seats[i];
+                }
+
+            return 0;
+        }
+    }
+}
