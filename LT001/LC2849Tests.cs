c16c26e503a7fb1e0f856e369a9c23e1e67c93ce
Holofiber <aminus2010@gmail.com>
2023-11-08 17:10:00 +0100

    LC2848 Accepted, Chebyshev distance

---
+++ LT001/LC2849Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Medium;
+using NUnit.Framework;
+
+namespace LeetCodeTest
+{
+    internal class LC2849Tests
+    {
+        [Test]
+        [TestCase(5, 1, 1, 4, 4, true)]
+        [TestCase(2, 1, 4, 3, 1, false)]
+        [TestCase(1, 4, 4, 1, 4, true)]
+        [TestCase(2, 4, 5, 2, 3, true)]
+        [TestCase(2, 4, 7, 7, 6, true)]
+        [TestCase(3, 1, 7, 3, 3, false)]
+        [TestCase(1, 2, 1, 2, 1, false)]
+        [TestCase(1, 1, 1, 1, 1, false)]
+        [TestCase(1, 3, 3, 5, 2, true)]
+        public void LC2038_ShouldBeTrue(int sx, int sy, int fx, int fy, int t, bool expected)
+        {
+            // Assert
+            var lc2849 = new LC2849();
+
+            var result = lc2849.IsReachableAtTime(sx, sy, fx, fy, t);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ LeetCode/Medium/LC2849.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.Medium
+{
+    //https://leetcode.com/problems/determine-if-a-cell-is-reachable-at-a-given-time/?envType=daily-question&envId=2023-11-08
+    public class LC2849
+    {
+        public bool IsReachableAtTime(int sx, int sy, int fx, int fy, int t)
+        {
+            // validate H case
+            if (fx == sx && fy == sy)
+                return t>1;
+
+
+            var deltaX = Math.Abs(fx-sx);
+            var deltaY = Math.Abs(fy-sy);
+
+            // Chebyshev distance
+            int max = Math.Max(deltaY, deltaX);
+
+            return t>= max;
+        }
+    }
+}
