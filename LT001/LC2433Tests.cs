faa2b24934a67481e90c5af63b48f819ce019809
Holofiber <aminus2010@gmail.com>
2023-11-09 15:05:47 +0100

    LC2433 Find The Original Array of Prefix Xor

---
+++ LT001/LC2433Tests.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Medium;
+using NUnit.Framework;
+
+namespace LeetCodeTest
+{
+    internal class LC2433Tests
+    {
+        [Test]
+        public void LC2433_ShouldBeTrue()
+        {
+            // Assert
+            var lc2433 = new LC2433();
+
+            int[] pref = new int[] { 5, 2, 0, 3, 1 };
+            int[] expected = new int[] { 5, 7, 2, 3, 2 };
+
+            var result = lc2433.FindArray(pref);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void LC2433_OneItem_ShouldBeTrue()
+        {
+            // Assert
+            var lc2433 = new LC2433();
+
+            int[] pref = new int[] { 5 };
+            int[] expected = new int[] { 5};
+
+            var result = lc2433.FindArray(pref);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ LeetCode/Medium/LC2433.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Medium
+{
+    public class LC2433
+    {
+        public int[] FindArray(int[] pref)
+        {
+            int[] result = new int[pref.Length];
+            result[0] = pref[0];
+            for (int i = 0; i < pref.Length-1; i++)
+            {
+                result[i+1] = pref[i] ^ pref[i+1];
+            }
+
+            return result;
+        }
+    }
+}
